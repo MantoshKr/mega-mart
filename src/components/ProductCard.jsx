045bd6f3a08a0d60fd21ed4f0be3b780a04ea9e1
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-11-18 21:40:07 +0530

    feat: Enabled individual product details page navigation on product name and description click

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -67,72 +67,73 @@ const ProductCard = ({
 
   return (
     <div>
-      <Link
-        href={{
-          pathname: `/product/${id}`,
-          query: {
-            title: title,
-            price: price,
-            description: description,
-            category: category,
-            image: image,
-            ratingcount: ratingcount,
-            rating: rating,
-          },
-        }}
-      >
-        <div className=" border-gray-200 bg-white lg:mb-4 lgl:mb-6 group">
-          <div className="w-full h-80 overflow-hidden p-6 relative">
-            <Image
-              className="w-full h-full object-contain scale-100 group-hover:scale-105  duration-300"
-              alt="img"
-              width={300}
-              height={250}
-              src={image}
-              loading="lazy"
-            />
-            <p
-              className="absolute top-4 right-4 text-2xl cursor-pointer"
-              onClick={toggleWishlist}
-            >
-              {isItemInWishlist ? (
-                <AiFillHeart style={{ color: "red" }} />
-              ) : (
-                <AiOutlineHeart />
-              )}
-            </p>
-          </div>
-          <div className="px-2 py-4  flex flex-col justify-center">
-            <div className="flex justify-between py-2">
-              <button
-                onClick={addItemToBasket}
-                className="w-20 h-9 bg-blue text-white rounded-full flex gap-1 items-center justify-center hover:bg-[#004f9a] duration-300 "
-              >
-                <span>
-                  <GoPlus />
-                </span>
-                Add
-              </button>
+      <div className=" border-gray-200 bg-white lg:mb-4 lgl:mb-6 group">
+        <div className="w-full h-80 overflow-hidden p-6 relative">
+          <Image
+            className="w-full h-full object-contain scale-100 group-hover:scale-105  duration-300"
+            alt="img"
+            width={300}
+            height={250}
+            src={image}
+            loading="lazy"
+          />
 
-              <Link
-                href={{
-                  pathname: `/product/${id}`,
-                  query: {
-                    title: title,
-                    price: price,
-                    description: description,
-                    category: category,
-                    image: image,
-                    ratingcount: ratingcount,
-                    rating: rating,
-                  },
-                }}
-              >
-                <button className="w-24 h-9 bg-white border-[1px] border-black gap-1 text-black rounded-full flex items-center justify-center hover:bg-black hover:text-white duration-300">
-                  Options
-                </button>
-              </Link>
-            </div>
+          <p
+            className="absolute top-4 right-4 text-2xl cursor-pointer"
+            onClick={toggleWishlist}
+          >
+            {isItemInWishlist ? (
+              <AiFillHeart style={{ color: "red" }} />
+            ) : (
+              <AiOutlineHeart />
+            )}
+          </p>
+        </div>
+        <div className="px-2 py-4  flex flex-col justify-center">
+          <div className="flex justify-between py-2">
+            <button
+              onClick={addItemToBasket}
+              className="w-20 h-9 bg-blue text-white rounded-full flex gap-1 items-center justify-center hover:bg-[#004f9a] duration-300 "
+            >
+              <span>
+                <GoPlus />
+              </span>
+              Add
+            </button>
+
+            <Link
+              href={{
+                pathname: `/product/${id}`,
+                query: {
+                  title: title,
+                  price: price,
+                  description: description,
+                  category: category,
+                  image: image,
+                  ratingcount: ratingcount,
+                  rating: rating,
+                },
+              }}
+            >
+              <button className="w-24 h-9 bg-white border-[1px] border-black gap-1 text-black rounded-full flex items-center justify-center hover:bg-black hover:text-white duration-300">
+                Options
+              </button>
+            </Link>
+          </div>
+          <Link
+            href={{
+              pathname: `/product/${id}`,
+              query: {
+                title: title,
+                price: price,
+                description: description,
+                category: category,
+                image: image,
+                ratingcount: ratingcount,
+                rating: rating,
+              },
+            }}
+          >
             <div className="flex item-center gap-3">
               <p className="font-titleFont text-lg text-green-700 font-semibold">
                 Now Rs {Math.floor(price * 83)}
@@ -143,26 +144,26 @@ const ProductCard = ({
             </div>
             <p>{title.substring(0, 25)}</p>
             <p>{description.substring(0, 70)}...</p>
-            <div className="flex gap-2 items-center text-sm mt-2 justify-between">
-              <div className="flex text-sm gap-1">
-                <RenderStars rating={rating} />
-                {ratingcount}
-              </div>
-              {hasPlus && (
-                <div className="font-bold text-green-600 flex justify-center items-center mx-2 w-8 cursor-pointer">
-                  <Image
-                    src="/assets/images/Mplus.svg"
-                    alt=""
-                    width={30}
-                    height={30}
-                    loading="lazy"
-                  />
-                </div>
-              )}
+          </Link>
+          <div className="flex gap-2 items-center text-sm mt-2 justify-between">
+            <div className="flex text-sm gap-1">
+              <RenderStars rating={rating} />
+              {ratingcount}
             </div>
+            {hasPlus && (
+              <div className="font-bold text-green-600 flex justify-center items-center mx-2 w-8 cursor-pointer">
+                <Image
+                  src="/assets/images/Mplus.svg"
+                  alt=""
+                  width={30}
+                  height={30}
+                  loading="lazy"
+                />
+              </div>
+            )}
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
